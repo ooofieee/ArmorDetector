96bf5054cdfa1b55b744c0c9bfd8fa61fa709222
ooofieee <ofinel188@gmail.com>
2024-12-04 16:44:20 +0800

    Create findNum.h

---
+++ TrackingTest/include/findNum.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <iostream>
+#include<opencv2/highgui.hpp>
+#include<opencv2/imgcodecs.hpp>
+#include<opencv2/imgproc.hpp>
+#include<cmath>
+
+using namespace std;
+using namespace cv;
+
+void findNum(Mat numUnchecked);
