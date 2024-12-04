7c9ede19b002e083377f6dc70f798be1bf78d6c4
ooofieee <ofinel188@gmail.com>
2024-12-04 16:44:17 +0800

    Create findLghtBar.h

---
+++ TrackingTest/include/findLghtBar.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <iostream>
+#include<opencv2/highgui.hpp>
+#include<opencv2/imgcodecs.hpp>
+#include<opencv2/imgproc.hpp>
+
+using namespace std;
+using namespace cv;
+
+void findLightBar(VideoCapture video);
